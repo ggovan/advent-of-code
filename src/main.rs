c984db6471cd0b709aa65d59bb2d52f383a5fb33
Gordon Govan <gordon@ggovan.uk>
2019-12-05 17:23:25 +0100

    Day 5

--- src/main.rs
+++ src/main.rs
@@ -16,9 +16,11 @@ fn main() -> Res<()> {
 
     day_2_part_1()?;
     day_2_part_2()?;
-    day_3_part_1()?;
+    day_3()?;
 
-    day_4_part_1();
+    day_4();
+
+    day_5()?;
 
     Ok(())
 }
@@ -68,36 +70,236 @@ where
 fn day_2_part_1() -> Res<()> {
     println!("Day 2");
 
-    let mut mem: Vec<usize> = read_better("day_2.in", &|s| s.parse::<usize>().unwrap())?
+    let mut mem: Vec<i32> = read_better("day_2.in", &|s| s.parse::<i32>().unwrap())?
         .nth(0)
         .unwrap();
 
     mem[1] = 12;
     mem[2] = 2;
 
-    run_to_completion(&mut mem, 0);
+    let mut machine = Machine {
+        mem,
+        op_index: 0,
+        input: vec![],
+        input_index: 0,
+        output: vec![],
+    };
 
-    println!("  part 1 {}", mem[0]);
+    machine.run_to_completion(0);
+
+    println!("  part 1 {}", machine.mem[0]);
     Ok(())
 }
 
+fn day_5() -> Res<()> {
+    println!("Day 5");
+
+    let mem: Vec<i32> = read_better("day_5.in", &|s| s.parse::<i32>().unwrap())?
+        .nth(0)
+        .unwrap();
+
+    let mut machine = Machine {
+        mem: mem.clone(),
+        op_index: 0,
+        input: vec![1],
+        input_index: 0,
+        output: vec![],
+    };
+    machine.run_to_completion(0);
+    println!("  part 1 {:?}", machine.output);
+
+    let mut machine_2 = Machine {
+        mem: mem.clone(),
+        op_index: 0,
+        input: vec![5],
+        input_index: 0,
+        output: vec![],
+    };
+    machine_2.run_to_completion(0);
+    println!("  part 2 {:?}", machine_2.output);
+    Ok(())
+}
+
+#[derive(Debug)]
+struct Machine {
+    mem: Vec<i32>,
+    op_index: usize,
+    input: Vec<i32>,
+    input_index: usize,
+    output: Vec<i32>,
+}
+
+#[derive(Debug)]
+enum Mode {
+    Immediate,
+    Address,
+}
+impl Mode {
+    fn from(val: i32) -> Self {
+        match val {
+            0 => Mode::Address,
+            _ => Mode::Immediate,
+        }
+    }
+}
+
+impl Machine {
+    fn ck_addr(&self, val: i32) -> usize {
+        if 0 <= val && val < self.mem.len() as i32 {
+            val as usize
+        } else {
+            panic!("Address is out of range {}!\n{:?}", val, self)
+        }
+    }
+
+    fn run_to_completion(&mut self, start_op_index: usize) {
+        let mut next = Some(start_op_index);
+        while let Some(_i) = next {
+            next = self.compute_step();
+        }
+    }
+
+    fn op_to_value(&self, op: i32, mode: Mode) -> i32 {
+        match mode {
+            Mode::Immediate => op as i32,
+            Mode::Address => self.mem[self.ck_addr(op)],
+        }
+    }
+
+    fn read_op_and_modes(&self) -> (usize, Mode, Mode, Mode) {
+        let raw_val = self.mem[self.op_index];
+        (
+            self.ck_addr(raw_val % 100),
+            Mode::from(raw_val / 100 % 10),
+            Mode::from(raw_val / 1000 % 10),
+            Mode::from(raw_val / 10000),
+        )
+    }
+
+    fn read_operands_1(&self, mode: Mode) -> (i32, usize) {
+        (
+            self.op_to_value(self.mem[self.op_index + 1], mode),
+            self.op_index + 2,
+        )
+    }
+    fn read_operands_2(&self, mode_1: Mode, mode_2: Mode) -> (i32, i32, usize) {
+        (
+            self.op_to_value(self.mem[self.op_index + 1], mode_1),
+            self.op_to_value(self.mem[self.op_index + 2], mode_2),
+            self.op_index + 3,
+        )
+    }
+    fn read_operands_3(&self, mode_1: Mode, mode_2: Mode, mode_3: Mode) -> (i32, i32, i32, usize) {
+        (
+            self.op_to_value(self.mem[self.op_index + 1], mode_1),
+            self.op_to_value(self.mem[self.op_index + 2], mode_2),
+            self.op_to_value(self.mem[self.op_index + 3], mode_3),
+            self.op_index + 4,
+        )
+    }
+
+    fn compute_step(&mut self) -> Option<usize> {
+        let (instruction, mode_1, mode_2, _mode_3) = self.read_op_and_modes();
+        let next = match instruction {
+            1 => {
+                let (val_1, val_2, dest, next) =
+                    self.read_operands_3(mode_1, mode_2, Mode::Immediate);
+                let dest = self.ck_addr(dest);
+                self.mem[dest] = val_1 + val_2;
+                self.op_index = next;
+                Some(self.op_index)
+            }
+            2 => {
+                let (val_1, val_2, dest, next) =
+                    self.read_operands_3(mode_1, mode_2, Mode::Immediate);
+                let dest = self.ck_addr(dest);
+                self.mem[dest] = val_1 * val_2;
+                self.op_index = next;
+                Some(self.op_index)
+            }
+            3 => {
+                let (dest, next) = self.read_operands_1(Mode::Immediate);
+                let dest = self.ck_addr(dest);
+                self.mem[dest] = self.input[self.input_index];
+                self.input_index += 1;
+                self.op_index = next;
+                Some(self.op_index)
+            }
+            4 => {
+                let (val_1, next) = self.read_operands_1(mode_1);
+                self.output.push(val_1);
+                self.op_index = next;
+                Some(self.op_index)
+            }
+            5 => {
+                let (val_1, val_2, maybe_next) = self.read_operands_2(mode_1, mode_2);
+                let next = if val_1 != 0 {
+                    self.ck_addr(val_2)
+                } else {
+                    maybe_next
+                };
+                self.op_index = next;
+                Some(self.op_index)
+            }
+            6 => {
+                let (val_1, val_2, maybe_next) = self.read_operands_2(mode_1, mode_2);
+                let next = if val_1 == 0 {
+                    self.ck_addr(val_2)
+                } else {
+                    maybe_next
+                };
+                self.op_index = next;
+                Some(self.op_index)
+            }
+            7 => {
+                let (val_1, val_2, dest, next) =
+                    self.read_operands_3(mode_1, mode_2, Mode::Immediate);
+                let dest = self.ck_addr(dest);
+                self.mem[dest] = if val_1 < val_2 { 1 } else { 0 };
+                self.op_index = next;
+                Some(self.op_index)
+            }
+            8 => {
+                let (val_1, val_2, dest, next) =
+                    self.read_operands_3(mode_1, mode_2, Mode::Immediate);
+                let dest = self.ck_addr(dest);
+                self.mem[dest] = if val_1 == val_2 { 1 } else { 0 };
+                self.op_index = next;
+                Some(self.op_index)
+            }
+            99 => Option::None,
+            x => panic!("Invalid instruction {}", x),
+        };
+        next
+    }
+}
+
 fn day_2_part_2() -> Res<()> {
-    let input: Vec<usize> = read_lines("day_2.in")?
+    let input: Vec<i32> = read_lines("day_2.in")?
         .nth(0)
         .unwrap()?
         .split(",")
         .map(|s| s.parse().unwrap())
         .collect();
 
-    let mut pairs: Vec<(usize, usize)> = vec![];
+    let mut pairs: Vec<(i32, i32)> = vec![];
 
     for noun in 0..100 {
         for verb in 0..100 {
             let mut mem = input.clone();
             mem[1] = noun;
             mem[2] = verb;
-            run_to_completion(&mut mem, 0);
-            if mem[0] == 19690720 {
+
+            let mut machine = Machine {
+                mem,
+                op_index: 0,
+                input: vec![],
+                input_index: 0,
+                output: vec![],
+            };
+
+            machine.run_to_completion(0);
+            if machine.mem[0] == 19690720 {
                 pairs.push((noun, verb))
             }
         }
@@ -107,40 +309,7 @@ fn day_2_part_2() -> Res<()> {
     Ok(())
 }
 
-fn run_to_completion(mem: &mut Vec<usize>, start_op_index: usize) {
-    let mut next = Some(start_op_index);
-    while let Some(i) = next {
-        next = compute_step(mem, i)
-    }
-}
-
-fn read_operands(input: &mut Vec<usize>, op_index: usize) -> (usize, usize, usize) {
-    (
-        input[op_index + 1],
-        input[op_index + 2],
-        input[op_index + 3],
-    )
-}
-
-fn compute_step(input: &mut Vec<usize>, op_index: usize) -> Option<usize> {
-    let next = match input[op_index] {
-        1 => {
-            let (op_1, op_2, dest) = read_operands(input, op_index);
-            input[dest] = input[op_1] + input[op_2];
-            Some(op_index + 4)
-        }
-        2 => {
-            let (op_1, op_2, dest) = read_operands(input, op_index);
-            input[dest] = input[op_1] * input[op_2];
-            Some(op_index + 4)
-        }
-        99 => Option::None,
-        x => panic!("Invalid instruction {}", x),
-    };
-    next
-}
-
-fn day_3_part_1() -> Res<()> {
+fn day_3() -> Res<()> {
     println!("Day 3");
     let input = read_better("day_3.in", &LineSeg::parse)?;
     let wires = input.map(|ss| Wire::from_segments(&ss)).collect::<Vec<_>>();
@@ -296,7 +465,7 @@ impl Wire {
     }
 }
 
-fn day_4_part_1() {
+fn day_4() {
     println!("Day 4");
     let from = 171309;
     let until = 643063;
