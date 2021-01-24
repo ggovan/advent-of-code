7ee66480898321fabfe3033fa533193999529fb1
Govan, Gordon <gordon@ggovan.uk>
2021-01-24 18:29:53 +0100

    2019 day 21

--- src/aoc_2019/day_21.rs
+++ src/aoc_2019/day_21.rs
@@ -1,4 +1,5 @@
 use crate::aoc_2020::Aoc2020;
+use crate::common::bitset::Bitset;
 use crate::files::Res;
 use itertools::Itertools;
 use std::convert::TryFrom;
@@ -10,7 +11,7 @@ pub struct Day21;
 impl Aoc2020 for Day21 {
     type Input = Vec<i64>;
     type Result1 = i64;
-    type Result2 = u64;
+    type Result2 = i64;
 
     fn day() -> usize {
         21
@@ -20,20 +21,19 @@ impl Aoc2020 for Day21 {
     }
 
     fn part_1(code: &Self::Input) -> Self::Result1 {
+        use Op::*;
         let input = [
-            "NOT C J", // if there is hole at C
-            "AND D J", // only jump if you can land
-            "NOT A T", // if not A then we must jump
-            "OR T J",  // OR the above two rules
-            "WALK",    //
-            "",        // add the new line at the end
-        ]
-        .iter()
-        .join("\n");
+            Rule(NOT, 'C', 'J'), // if there is hole at C
+            Rule(AND, 'D', 'J'), // only jump if you can land
+            Rule(NOT, 'A', 'T'), // if not A then we must jump
+            Rule(OR, 'T', 'J'),  // OR the above two rules
+        ];
 
+        let mut input = input.iter().map(Rule::to_string).join("\n");
+        input.push_str("\nWALK\n");
         let input: Vec<i64> = input.bytes().map(|b| b as i64).collect();
-        let mut machine = Machine::new(code, input);
 
+        let mut machine = Machine::new(code, input);
         machine.run_to_completion();
 
         let result = machine.output.iter().find(|&&b| u8::try_from(b).is_err());
@@ -48,7 +48,211 @@ impl Aoc2020 for Day21 {
         }
     }
 
-    fn part_2(_input: &Self::Input) -> Self::Result2 {
-        21
+    fn part_2(code: &Self::Input) -> Self::Result2 {
+        use Op::*;
+        let input = [
+            // is_gap -> J
+            Rule(NOT, 'B', 'J'), // no B
+            Rule(NOT, 'C', 'T'), // np C
+            Rule(OR, 'T', 'J'),  // either b and c are holes
+            // is gap && D -> J
+            Rule(AND, 'D', 'J'), //
+            // is gap && D && (E || H) -> J (can_jump)
+            Rule(NOT, 'A', 'T'), // clear T to false
+            Rule(OR, 'E', 'T'),  // if e
+            Rule(OR, 'H', 'T'),  // ... or h
+            Rule(AND, 'T', 'J'), // && is_gap && D
+            // must_jump
+            Rule(NOT, 'A', 'T'), //
+            // can_jump || must_jump
+            Rule(OR, 'T', 'J'), //
+        ];
+
+        let mut input = input.iter().map(Rule::to_string).join("\n");
+        input.push_str("\nRUN\n");
+        let input: Vec<i64> = input.bytes().map(|b| b as i64).collect();
+
+        let mut machine = Machine::new(code, input);
+        machine.run_to_completion();
+
+        let result = machine.output.iter().find(|&&b| u8::try_from(b).is_err());
+
+        if let Some(res) = result {
+            *res
+        } else {
+            let output: String = machine.output.iter().map(|&b| b as u8 as char).collect();
+            let output = output.lines().skip(7).join("\n");
+
+            println!("Output: \n{}", output);
+            println!("-ABCDEFGHI");
+            -1
+        }
+    }
+}
+
+#[allow(unused)]
+fn run_springstep<const N: usize>(rules: &[Rule], input: Bitset, log: bool) -> bool {
+    use Op::*;
+
+    let mut j = false;
+    let mut t = false;
+
+    for r in rules {
+        let p1 = if ('A'..='I').contains(&r.1) {
+            input.contains((r.1 as u8 - b'A') as usize)
+        } else if r.1 == 'T' {
+            t
+        } else {
+            j
+        };
+
+        let p2 = if r.2 == 'T' { &mut t } else { &mut j };
+        let tp = (p1, *p2);
+
+        match r.0 {
+            AND => *p2 &= p1,
+            OR => *p2 |= p1,
+            NOT => *p2 = !p1,
+        };
+        if log {
+            println!("{:?}, (p1, p2): {:?}, T: {}, J: {}", r, tp, t, j);
+        }
+    }
+
+    if log {
+        println!("@\n#{} : {}", format_path::<N>(input), j);
+    }
+
+    j
+}
+
+#[derive(Copy, Clone, std::fmt::Debug, Eq, PartialEq)]
+enum Op {
+    AND,
+    OR,
+    NOT,
+}
+
+#[derive(Copy, Clone, std::fmt::Debug, Eq, PartialEq)]
+struct Rule(Op, char, char);
+
+impl std::fmt::Display for Rule {
+    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+        write!(f, "{:?} {} {}", self.0, self.1, self.2)
+    }
+}
+
+fn format_path<const N: usize>(input: Bitset) -> String {
+    (0..N)
+        .map(|i| if input.contains(i) { '#' } else { '.' })
+        .collect()
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn jumping() {
+        use Op::*;
+
+        let input = [
+            // is_gap -> J
+            Rule(NOT, 'B', 'J'), // no B
+            Rule(NOT, 'C', 'T'), // np C
+            Rule(OR, 'T', 'J'),  // either b and c are holes
+            // is gap && D -> J
+            Rule(AND, 'D', 'J'), // t iff should jump from b
+            // is gap && D && (E || H) -> J
+            Rule(NOT, 'A', 'T'),
+            Rule(OR, 'E', 'T'), // t if we shouldn't jump then
+            Rule(OR, 'H', 'T'), // we jump now
+            Rule(AND, 'T', 'J'),
+            // must_jump
+            Rule(NOT, 'A', 'T'), //
+            // jump
+            Rule(OR, 'T', 'J'), //
+        ];
+
+        let failed_jumps = test_all::<9>(
+            &input,
+            &[
+                //ABCD EFGH I
+                0b0111_1000_0, //
+                // 0b1001_0000_0, // need e or H
+                0b1001_1000_0, //
+                0b1001_0001_0, //
+                0b1101_0011_1, //
+                0b1101_1101_0, //
+            ],
+            true,
+        );
+
+        let failed_walks = test_all::<9>(
+            &input,
+            &[
+                //ABCD EFGH I
+                0b1111_0101_1, //
+                0b1110_1011_0, //
+                0b1101_0110_1, //
+                0b1101_0100_1, //
+            ],
+            false,
+        );
+
+        assert!(!failed_walks && !failed_jumps);
+    }
+
+    fn test_all<const N: usize>(prog: &[Rule], paths: &[u64], is_jumping: bool) -> bool {
+        let fails = paths
+            .iter()
+            .map(|&i| Bitset::from(i).flip_start(N))
+            .filter(|&i| run_springstep::<N>(prog, i, false) != is_jumping)
+            .map(format_path::<N>)
+            .collect::<Vec<_>>();
+
+        if !fails.is_empty() {
+            println!(
+                "Should have {}:\n{}",
+                if is_jumping { "jumped" } else { "walked" },
+                fails.iter().join("\n")
+            );
+            true
+        } else {
+            false
+        }
+    }
+
+    #[test]
+    fn test_is_gap() {
+        use Op::*;
+        let input = [
+            // is_gap (J)
+            Rule(NOT, 'B', 'J'), // no B
+            Rule(NOT, 'C', 'T'), // np C
+            Rule(OR, 'T', 'J'),  // either b and c are holes
+        ];
+
+        let failed_jumps = test_all::<4>(
+            &input,
+            &[
+                // 0b0111, // this is covered by not_a
+                0b1001, //
+                0b1101, //
+                0b1011, //
+            ],
+            true,
+        );
+
+        let failed_walks = test_all::<4>(
+            &input,
+            &[
+                0b0111, //
+                0b1111, //
+            ],
+            false,
+        );
+
+        assert!(!failed_walks && !failed_jumps);
     }
 }
--- src/common/bitset.rs
+++ src/common/bitset.rs
@@ -1,15 +1,22 @@
 #[derive(PartialEq, Eq, Copy, Clone, std::fmt::Debug, Hash)]
 pub struct Bitset(u64);
 
+#[allow(unused)]
 impl Bitset {
     pub fn empty() -> Self {
         Bitset(0)
     }
 
+    pub fn from(raw: u64) -> Self {
+        Bitset(raw)
+    }
+
+    #[must_use]
     pub fn set(&self, position: usize) -> Self {
         Self(self.0 | (1 << position))
     }
 
+    #[must_use]
     pub fn clear(&self, position: usize) -> Self {
         Self(self.0 & (!0 ^ (1 << position)))
     }
@@ -24,5 +31,29 @@ impl Bitset {
 
     pub fn contains_all(&self, Bitset(other): Bitset) -> bool {
         (self.0 & other) == other
+    }
+
+    #[must_use]
+    pub fn flip_start(&self, len: usize) -> Self {
+        let mut new = Bitset::empty();
+        for i in 0..len {
+            if self.contains(i) {
+                new = new.set(len - i - 1);
+            }
+        }
+        new
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn flip_start() {
+        assert_eq!(0b0, Bitset::from(0).flip_start(1).0);
+        assert_eq!(0b1, Bitset::from(0b1).flip_start(1).0);
+        assert_eq!(0b01, Bitset::from(0b10).flip_start(2).0);
+        assert_eq!(0b01, Bitset::from(0b10).flip_start(2).0);
     }
 }
