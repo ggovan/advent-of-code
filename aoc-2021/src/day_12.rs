7e14b50fff3cf8c5d95a748de530e030853e6b91
Gordon Govan <gordon@ggovan.uk>
2021-12-16 22:00:28 +0100

    2021 Day 12

--- aoc-2021/src/day_12.rs
+++ aoc-2021/src/day_12.rs
@@ -5,158 +5,101 @@ use std::fs::read_to_string;
 pub struct Day12;
 
 impl AocDay for Day12 {
-    type Input = Vec<Action>;
-    type Result1 = i32;
-    type Result2 = i32;
+    type Input = Vec<(String, String)>;
+    type Result1 = usize;
+    type Result2 = usize;
 
     fn day() -> usize {
         12
     }
     fn load() -> Res<Self::Input> {
-        Ok(read_to_string("data/2020/day_12.in")?
+        Ok(read_to_string("data/2021/day_12.in")?
             .lines()
-            .map(Action::from)
+            .flat_map(parse)
             .collect())
     }
 
-    /// Move the ship around, nothing fancy here.
     fn part_1(input: &Self::Input) -> Self::Result1 {
-        let mut dir = Direction::Right;
-        let mut pos = (0, 0);
-
-        for &command in input {
-            match command {
-                N(amount) => pos = Direction::Up.move_along(pos, amount),
-                S(amount) => pos = Direction::Down.move_along(pos, amount),
-                W(amount) => pos = Direction::Left.move_along(pos, amount),
-                E(amount) => pos = Direction::Right.move_along(pos, amount),
-                F(amount) => pos = dir.move_along(pos, amount),
-                R(amount) => dir = dir.rotate_cw(amount),
-                L(amount) => dir = dir.rotate_acw(amount),
-            }
-        }
-
-        pos.0.abs() + pos.1.abs()
+        find_all_routes(input, &mut vec!["start".to_owned()], true)
     }
 
-    /// Move the ship around, with some fun rotation of the waypoint.
     fn part_2(input: &Self::Input) -> Self::Result2 {
-        let mut pos = (0, 0);
-        let mut way = (10, -1);
-
-        for &command in input {
-            match command {
-                N(amount) => way = Direction::Up.move_along(way, amount),
-                S(amount) => way = Direction::Down.move_along(way, amount),
-                W(amount) => way = Direction::Left.move_along(way, amount),
-                E(amount) => way = Direction::Right.move_along(way, amount),
-                F(amount) => pos = move_to_waypoint(pos, &way, amount),
-                R(amount) => way = rotate_cw(way, amount),
-                L(amount) => way = rotate_acw(way, amount),
-            }
-        }
-
-        pos.0.abs() + pos.1.abs()
+        find_all_routes(input, &mut vec!["start".to_owned()], false)
     }
 }
 
-fn move_to_waypoint((x, y): Point, &(wx, wy): &Point, amount: i32) -> Point {
-    (x + wx * amount, y + wy * amount)
+fn parse(line: &str) -> Vec<(String, String)> {
+    let (left, right) = line.split_once("-").unwrap();
+    vec![
+        (left.to_owned(), right.to_owned()),
+        (right.to_owned(), left.to_owned()),
+    ]
 }
 
-fn rotate_cw((x, y): Point, deg: i32) -> Point {
-    match deg {
-        1 => (-y, x),
-        2 => (-x, -y),
-        3 => (y, -x),
-        _ => unreachable!("Impossible: {}", deg),
-    }
-}
+fn find_all_routes(
+    paths: &[(String, String)],
+    from: &mut Vec<String>,
+    visited_small: bool,
+) -> usize {
+    if *from.last().unwrap() == "end" {
+        1
+    } else {
+        paths
+            .iter()
+            .filter_map(|(start, end)| {
+                if start != from.last().unwrap() || "start" == end {
+                    None
+                } else {
+                    let big = is_big(end);
+                    let unseen = big || unseen(end, from);
 
-fn rotate_acw((x, y): Point, deg: i32) -> Point {
-    match deg {
-        1 => (y, -x),
-        2 => (-x, -y),
-        3 => (-y, x),
-        _ => unreachable!("Impossible: {}", deg),
-    }
-}
-
-type Point = (i32, i32);
-
-use Action::*;
-#[derive(Copy, Clone)]
-pub enum Action {
-    N(i32),
-    E(i32),
-    S(i32),
-    W(i32),
-    R(i32),
-    L(i32),
-    F(i32),
-}
-
-impl Action {
-    fn from(s: &str) -> Self {
-        let command = s.chars().next().unwrap();
-        let amount = s
-            .chars()
-            .skip(1)
-            .collect::<String>()
-            .parse::<i32>()
-            .unwrap();
-
-        match command {
-            'N' => N(amount),
-            'S' => S(amount),
-            'W' => W(amount),
-            'E' => E(amount),
-            'F' => F(amount),
-            'R' => R(amount / 90),
-            'L' => L(amount / 90),
-            c => unreachable!("WHAT! {}", c),
-        }
+                    if big || unseen || !visited_small {
+                        from.push(end.clone());
+                        let sum = find_all_routes(
+                            paths,
+                            from,
+                            if big {
+                                visited_small
+                            } else {
+                                !unseen || visited_small
+                            },
+                        );
+                        from.pop();
+                        Some(sum)
+                    } else {
+                        None
+                    }
+                }
+            })
+            .sum()
     }
 }
 
-// TODO this is the third or so time I've written this!
-#[derive(Debug)]
-enum Direction {
-    Left,
-    Up,
-    Right,
-    Down,
+fn is_big(s: &str) -> bool {
+    s.chars().all(|c| c.is_uppercase())
 }
-impl Direction {
-    fn from(v: i32) -> Self {
-        match v {
-            0 => Direction::Up,
-            1 => Direction::Right,
-            2 => Direction::Down,
-            3 => Direction::Left,
-            _ => panic!("Unknown direction {}", v),
-        }
-    }
-    fn to_int(&self) -> i32 {
-        match self {
-            Direction::Up => 0,
-            Direction::Right => 1,
-            Direction::Down => 2,
-            Direction::Left => 3,
-        }
-    }
-    fn rotate_cw(&self, dir: i32) -> Self {
-        Self::from((self.to_int() + dir) % 4)
-    }
-    fn rotate_acw(&self, dir: i32) -> Self {
-        Self::from((self.to_int() + 4 - dir) % 4)
-    }
-    fn move_along(&self, (x, y): (i32, i32), amount: i32) -> (i32, i32) {
-        match self {
-            Direction::Up => (x, y - amount),
-            Direction::Right => (x + amount, y),
-            Direction::Down => (x, y + amount),
-            Direction::Left => (x - amount, y),
-        }
+
+fn unseen(s: &str, path: &[String]) -> bool {
+    path.iter().all(|e| e != s)
+}
+
+#[cfg(test)]
+mod tests {
+    use std::collections::HashMap;
+
+    use super::*;
+
+    #[test]
+    fn test_parse_input() {
+        let res = parse(
+            r#"start-A"#
+// start-b
+// A-c
+// A-b
+// b-d
+// A-end
+// b-end"#,
+        );
+        assert_eq!(res, ("start".to_owned(), "b".to_owned()));
     }
 }
